38f1fcece0883ffb988f7cfd03b1f78ce2a3df48
李宝 <libao@libaos-MacBook-Pro.local>
2018-03-31 12:00:52 +0800

    add sortdemo

---
+++ sortdemo/sort.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"sort"
+	"fmt"
+)
+
+func main() {
+	//create a slice of int
+	a := []int {3, 6, 5, 2, 7}
+	sort.Ints(a)
+
+	for i, v := range a {
+		fmt.Println(i, v)
+	}
+}
